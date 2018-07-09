0c7e0cf5c38798c2c64aa5bf79cb34262f29bf83
chaifangming <fangming.chai@huawei.com>
2018-07-09 20:26:41 +0800

    small footprint support for NRF51822

--- targets/NRF51822_BLE/Src/main.c
+++ targets/NRF51822_BLE/Src/main.c
@@ -34,56 +34,88 @@
 
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
-#include "nrf_rtc.h"
 #include "simple_uart.h"
 #include "main.h"
+#include "los_swtmr.h"
+#include "los_mux.h"
+#include "los_mux.ph"
 
-static UINT32 s_TaskHandle;
+/* 为了更小的代码尺寸请定义NO_UART,并且去除所有串口输出操作 */
+#define NO_UART
+#if 1
+#undef  NO_UART
+#endif
 
 static void HardWare_Init(void)
 {
     nrf_gpio_range_cfg_output(LED_START,LED_STOP);
-    simple_uart_config(RTS_PIN_NUMBER, TX_PIN_NUMBER, CTS_PIN_NUMBER, RX_PIN_NUMBER, HWFC);
+#ifndef NO_UART
+    simple_uart_config(RTS_PIN_NUMBER, TX_PIN_NUMBER, CTS_PIN_NUMBER, RX_PIN_NUMBER);
+#endif
 }
+
+UINT16 semHandler1, semHandler2, semHandler3;
+
+#if (LOSCFG_BASE_IPC_MUX == YES)
+UINT32 muxHandler;
+#endif
 
 static void task1(void)
 {
-    int count = 0;
-    
     while (true)
     {
-        nrf_gpio_pin_set(LED_0);
-        nrf_gpio_pin_set(LED_1);
-        nrf_delay_ms(300);
-        nrf_gpio_pin_clear(LED_0);
-        nrf_gpio_pin_clear(LED_1);
-        nrf_delay_ms(300);
-        printf("[%d] This is Task1++++++++++\n", ++count);
-        LOS_TaskDelay(100);
+#ifndef NO_UART
+        simple_uart_put ('1');
+#endif
+
+#if (LOSCFG_BASE_IPC_MUX == YES)
+        LOS_MuxPend (muxHandler, LOS_WAIT_FOREVER);
+#endif
+        LOS_SemPend (semHandler2, LOS_WAIT_FOREVER);
     }
 }
 
 static void task2(void)
 {
-    int count = 0;
-
     while (true)
     {
-        nrf_gpio_pin_set(LED_2);
-        nrf_gpio_pin_set(LED_3);
-        nrf_gpio_pin_set(LED_4);
-        nrf_delay_ms(300);
-        nrf_gpio_pin_clear(LED_2);
-        nrf_gpio_pin_clear(LED_3);
-        nrf_gpio_pin_clear(LED_4);
-        nrf_delay_ms(300);
-        printf("[%d] This is Task2----------\n", ++count);
+#ifndef NO_UART
+        simple_uart_put ('2');
+#endif
+        LOS_TaskDelay(100);
+#if (LOSCFG_BASE_IPC_MUX == YES)
+        LOS_MuxPost (muxHandler);
+#endif
+        LOS_SemPost (semHandler2);
+    }
+}
+
+static void task3(void)
+{
+    while (true)
+    {
+#ifndef NO_UART
+        simple_uart_put ('3');
+#endif
         LOS_TaskDelay(100);
     }
 }
 
-static UINT32 creat_tasks(void)
+#if (LOSCFG_STATIC_TASK == YES)
+LOS_TASK_DEF (task1, "t1", task1, 1, 0, 0x100);
+LOS_TASK_DEF (task2, "t2", task2, 2, 0, 0x100);
+LOS_TASK_DEF (task3, "t3", task3, 3, 0, 0x100);
+#endif
+
+static void creat_tasks(void)
 {
+    UINT32 taskHandle;
+
+#if (LOSCFG_STATIC_TASK == YES)
+    LOS_TASK_INIT (task1, &taskHandle);
+    LOS_TASK_INIT (task2, &taskHandle);
+    LOS_TASK_INIT (task3, &taskHandle);
+#else
     UINT32 uwRet = LOS_OK;
     TSK_INIT_PARAM_S task_init_param;
 
@@ -92,17 +124,92 @@ static UINT32 creat_tasks(void)
     task_init_param.pfnTaskEntry = (TSK_ENTRY_FUNC)task1;
     task_init_param.uwStackSize = 0x400;
 
-    uwRet = LOS_TaskCreate(&s_TaskHandle, &task_init_param);
-    if (LOS_OK != uwRet)
-    {
-        return uwRet;
-    }
+    LOS_TaskCreate(&taskHandle, &task_init_param);
 
     task_init_param.usTaskPrio = 1;
     task_init_param.pcName = "task2";
     task_init_param.pfnTaskEntry = (TSK_ENTRY_FUNC)task2;
 
-    return LOS_TaskCreate(&s_TaskHandle, &task_init_param);
+    LOS_TaskCreate(&taskHandle, &task_init_param);
+
+    task_init_param.usTaskPrio = 2;
+    task_init_param.pcName = "task3";
+    task_init_param.pfnTaskEntry = (TSK_ENTRY_FUNC)task3;
+
+    LOS_TaskCreate(&taskHandle, &task_init_param);
+#endif
+}
+
+void tmrHandler (char arg)
+{
+#ifndef NO_UART
+    simple_uart_put (arg);
+#endif
+}
+
+#if (LOSCFG_STATIC_TIMER == YES)
+LOS_TIMER_DEF (tmr1, 10, LOS_SWTMR_MODE_PERIOD, tmrHandler, 'a');
+LOS_TIMER_DEF (tmr2, 5,  LOS_SWTMR_MODE_PERIOD, tmrHandler, 'b');
+LOS_TIMER_DEF (tmr3, 20, LOS_SWTMR_MODE_PERIOD, tmrHandler, 'c');
+LOS_TIMER_DEF (tmr4, 40, LOS_SWTMR_MODE_PERIOD, tmrHandler, 'd');
+LOS_TIMER_DEF (tmr5, 80, LOS_SWTMR_MODE_PERIOD, tmrHandler, 'e');
+#endif
+
+static void create_tmr (void)
+{
+    UINT16 tmr1, tmr2, tmr3, tmr4, tmr5;
+
+#if (LOSCFG_STATIC_TIMER == YES)
+    LOS_TIMER_INIT (tmr1, &tmr1);
+    LOS_TIMER_INIT (tmr2, &tmr2);
+    LOS_TIMER_INIT (tmr3, &tmr3);
+    LOS_TIMER_INIT (tmr4, &tmr4);
+    LOS_TIMER_INIT (tmr5, &tmr5);
+#else
+    LOS_SwtmrCreate (10, 1, (SWTMR_PROC_FUNC) tmrHandler, &tmr1, (UINT32) 'a');
+    LOS_SwtmrCreate (5,  1, (SWTMR_PROC_FUNC) tmrHandler, &tmr2, (UINT32) 'b');
+    LOS_SwtmrCreate (20, 1, (SWTMR_PROC_FUNC) tmrHandler, &tmr3, (UINT32) 'c');
+    LOS_SwtmrCreate (40, 1, (SWTMR_PROC_FUNC) tmrHandler, &tmr4, (UINT32) 'd');
+    LOS_SwtmrCreate (80, 1, (SWTMR_PROC_FUNC) tmrHandler, &tmr5, (UINT32) 'e');
+#endif
+
+    LOS_SwtmrStart (tmr1);
+    LOS_SwtmrStart (tmr2);
+    LOS_SwtmrStart (tmr3);
+    LOS_SwtmrStart (tmr4);
+    LOS_SwtmrStart (tmr5);
+}
+
+#if (LOSCFG_STATIC_SEM == YES)
+LOS_SEM_DEF (sem1, 0);
+LOS_SEM_DEF (sem2, 0);
+LOS_BIN_SEM_DEF (sem3, 0);
+#endif
+
+static void create_sem (void)
+{
+#if (LOSCFG_STATIC_SEM == NO)
+    LOS_SemCreate (0, &semHandler1);
+    LOS_SemCreate (0, &semHandler2);
+    LOS_SemCreate (0, &semHandler3);
+#else
+    LOS_SEM_INIT (sem1, &semHandler1);
+    LOS_SEM_INIT (sem2, &semHandler2);
+    LOS_BIN_SEM_INIT (sem3, &semHandler3);
+#endif
+}
+
+static void create_mux (void)
+{
+#if (LOSCFG_BASE_IPC_MUX == YES)
+#if (LOSCFG_STATIC_MUX == YES)
+    LOS_MUX_DEF (test);
+
+    LOS_MUX_INIT (test, &muxHandler);
+#else
+    LOS_MuxCreate (&muxHandler);
+#endif
+#endif
 }
 
 /**
@@ -112,19 +219,19 @@ int main(void)
 {
     HardWare_Init();
 
-    printf ("Wellcome to Huawei LiteOS!\n");
-
     if (LOS_KernelInit() != LOS_OK)
     {
         return LOS_NOK;
     }
 
-    if (creat_tasks() != LOS_OK)
-    {
-        return LOS_NOK;
-    }
+    create_mux ();
 
-    nrf_rtc_init();         /* 51822 do not have SysTick, use RTC as tick timer */
+    creat_tasks();
+
+    create_tmr();
+
+    create_sem();
+
     LOS_Start();
 }
 
@@ -141,7 +248,7 @@ FILE __stdout;
 
 void _sys_exit(int x)
 {
-    x = x;
+    (void) x;
 }
 
 int fputc(int ch, FILE *f)
